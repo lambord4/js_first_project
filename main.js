215004f8a9e11334336f8047c09d5772f4bc9e62
lambord4 <molekyla25@gmail.com>
2025-02-07 12:46:57 +0200

    Выполнил домашние задание 3

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log (typeof 50);
+console.log (typeof "50");
+console.log (typeof 501238971298371293619286391872398172);
+console.log (typeof true);
+console.log (typeof undefined);
+console.log (typeof null);
+console.log (typeof Symbol());
+let user = {
+    name: "Ivan",
+    age: 25
+};
+console.log (typeof user);
+
+let numbers = [1, 2, 3, 4];
+console.log (typeof numbers);
+
+function sayHi() {
+    console.log("Hello World")
+};
+console.log (typeof sayHi)
